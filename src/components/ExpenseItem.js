6d19c70ad3719b40f6cabb03ff3174a437389f55
A. Aniebiet <aniebiet.akpan@gmail.com>
2022-12-24 17:37:59 +0000

    update

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -25,12 +25,26 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
 
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{background: 'green', borderRadius: '50px', fontWeight: 'bold', color: 'white', fontStyle: 'bold', fontSize: '18px'}} onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button style={{background: 'red', fontWeight: 'bold', borderRadius: '50px', color: 'white', fontStyle: 'bold', fontSize: '18px'}} onClick={event=> decreaseAllocation(props.name)}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
